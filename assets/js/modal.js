a9a3f90268ba4bf5842d968005788533898feb70
FunkyCss <62518751+FunkyCss@users.noreply.github.com>
2024-02-27 10:31:23 +0200

    Add class to close cancel button on modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,14 +1,23 @@
+
+document.addEventListener("DOMContentLoaded", function() {
   const section = document.querySelector(".modal"),
-        overlay = document.querySelector(".overlay"),
-        showBtn = document.querySelector(".show-modal"),
-        closeBtn = document.querySelector(".close-btn");
+  overlay = document.querySelector(".overlay"),
+  showBtn = document.querySelector(".show-modal"),
+  closeBtn = document.querySelector(".close-btn");
+  cancelBtn = document.querySelector(".cancel-btn");
 
-      showBtn.addEventListener("click", () => section.classList.add("active"));
+  showBtn.addEventListener("click", () => section.classList.add("active"));
 
-      overlay.addEventListener("click", () =>
-        section.classList.remove("active")
-      );
+  overlay.addEventListener("click", () =>
+  section.classList.remove("active")
+  );
 
-      closeBtn.addEventListener("click", () =>
-        section.classList.remove("active")
-      );
+  closeBtn.addEventListener("click", () =>
+  section.classList.remove("active")
+  );
+
+  cancelBtn.addEventListener("click", () =>
+  section.classList.remove("active")
+  );
+
+});
