6bdd2865238dd5b80445d9f92c0018dc0697a388
FunkyCss <62518751+FunkyCss@users.noreply.github.com>
2024-02-29 19:42:24 +0200

    Multistep form

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,23 +1,14 @@
-
-document.addEventListener("DOMContentLoaded", function() {
   const section = document.querySelector(".modal"),
-  overlay = document.querySelector(".overlay"),
-  showBtn = document.querySelector(".show-modal"),
-  closeBtn = document.querySelector(".close-btn");
-  cancelBtn = document.querySelector(".cancel-btn");
+        overlay = document.querySelector(".overlay"),
+        showBtn = document.querySelector(".show-modal"),
+        closeBtn = document.querySelector(".close-btn");
 
-  showBtn.addEventListener("click", () => section.classList.add("active"));
+      showBtn.addEventListener("click", () => section.classList.add("active"));
 
-  overlay.addEventListener("click", () =>
-  section.classList.remove("active")
-  );
+      overlay.addEventListener("click", () =>
+        section.classList.remove("active")
+      );
 
-  closeBtn.addEventListener("click", () =>
-  section.classList.remove("active")
-  );
-
-  cancelBtn.addEventListener("click", () =>
-  section.classList.remove("active")
-  );
-
-});
+      closeBtn.addEventListener("click", () =>
+        section.classList.remove("active")
+      );
