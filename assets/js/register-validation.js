b63734534f92b86825aa5d6a563fd9f76f5039d9
FunkyCss <62518751+FunkyCss@users.noreply.github.com>
2024-03-01 17:49:54 +0200

    Fix All Validations

--- assets/js/register-validation.js
+++ assets/js/register-validation.js
@@ -16,6 +16,7 @@ form.onsubmit = (e)=>{
   setTimeout(()=>{ //remove shake class after 500ms
     eField.classList.remove("shake");
     pField.classList.remove("shake");
+    nField.classList.remove("shake");
   }, 500);
 
   eInput.onkeyup = ()=>{checkEmail();} 
@@ -52,9 +53,6 @@ form.onsubmit = (e)=>{
       nField.classList.remove("valid");
       let errorTxt = nField.querySelector(".error-txt");
       (nInput.value != "") ? errorTxt.innerText = "Please enter a valid name" : errorTxt.innerText = "Name can't  be blank"; 
-    }else{ 
-      nField.classList.remove("error");
-      nField.classList.add("valid");
     }
   }
 
@@ -64,3 +62,43 @@ form.onsubmit = (e)=>{
     window.location.href = form.getAttribute("action"); //redirecting user to the specified url which is inside action attribute of form tag
   }
 }
+
+
+const passwordInput = document.querySelector(".pass-field input");
+const eyeIcon = document.querySelector(".pass-field i");
+const requirementList = document.querySelectorAll(".requirement-list li");
+
+// An array of password requirements with corresponding 
+// regular expressions and index of the requirement list item
+const requirements = [
+    { regex: /.{8,}/, index: 0 }, // Minimum of 8 characters
+    { regex: /[0-9]/, index: 1 }, // At least one number
+    { regex: /[a-z]/, index: 2 }, // At least one lowercase letter
+    { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
+    { regex: /[A-Z]/, index: 4 }, // At least one uppercase letter
+]
+
+passwordInput.addEventListener("keyup", (e) => {
+    requirements.forEach(item => {
+        // Check if the password matches the requirement regex
+        const isValid = item.regex.test(e.target.value);
+        const requirementItem = requirementList[item.index];
+
+        // Updating class and icon of requirement item if requirement matched or not
+        if (isValid) {
+            requirementItem.classList.add("valid");
+            requirementItem.firstElementChild.className = "fa-solid fa-check";
+        } else {
+            requirementItem.classList.remove("valid");
+            requirementItem.firstElementChild.className = "fa-solid fa-circle";
+        }
+    });
+});
+
+eyeIcon.addEventListener("click", () => {
+    // Toggle the password input type between "password" and "text"
+    passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+
+    // Update the eye icon class based on the password input type
+    eyeIcon.className = `fa-solid fa-eye${passwordInput.type === "password" ? "" : "-slash"}`;
+});
