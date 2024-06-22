2734ef1f3a1b71d01d8422e18d7f442da1a6f82a
FunkyCss <62518751+FunkyCss@users.noreply.github.com>
2024-06-22 13:24:49 +0300

    Implement Bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-const body = document.querySelector("body"),
-      modeToggle = body.querySelector(".mode-toggle");
-      sidebar = body.querySelector("nav");
-      sidebarToggle = body.querySelector(".sidebar-toggle");
-
-let getMode = localStorage.getItem("mode");
-if(getMode && getMode ==="dark"){
-    body.classList.toggle("dark");
-}
-
-let getStatus = localStorage.getItem("status");
-if(getStatus && getStatus ==="close"){
-    sidebar.classList.toggle("close");
-}
-
-modeToggle.addEventListener("click", () =>{
-    body.classList.toggle("dark");
-    if(body.classList.contains("dark")){
-        localStorage.setItem("mode", "dark");
-    }else{
-        localStorage.setItem("mode", "light");
-    }
-});
-
-sidebarToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-    if(sidebar.classList.contains("close")){
-        localStorage.setItem("status", "close");
-    }else{
-        localStorage.setItem("status", "open");
-    }
-})
-
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.sidebar-toggle').addEventListener('click', function () {
+        const sidebar = document.querySelector('#sidebar');
+        sidebar.classList.toggle('active');
+        if (sidebar.classList.contains('active')) {
+            sidebar.style.width = '250px';
+        } else {
+            sidebar.style.width = '50px';
+        }
+    });
 });
